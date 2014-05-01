cb98625e55927b62a4d1e79a005752604249024c
vossjck <vossjck@0f523e2a-70e0-4643-9f7c-8b595f397346>
2014-05-01 18:29:53 +0000

    test

---
+++ LarpPortal/trunk/LarpPortal/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LarpPortal.Startup))]
+namespace LarpPortal
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
