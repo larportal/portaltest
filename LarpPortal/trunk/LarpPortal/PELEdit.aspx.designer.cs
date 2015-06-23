bb10ee07815afe04ab24f3f6c62a88059dae40f4
jbradshaw <jbradshaw@0f523e2a-70e0-4643-9f7c-8b595f397346>
2015-06-23 00:50:14 +0000

    Added staff comments and CP awarded to edit page.

--- LarpPortal/trunk/LarpPortal/PELEdit.aspx.designer.cs
+++ LarpPortal/trunk/LarpPortal/PELEdit.aspx.designer.cs
@@ -67,6 +67,15 @@ namespace LarpPortal {
         protected global::System.Web.UI.WebControls.HiddenField hidTextBoxEnabled;
         
         /// <summary>
+        /// divQuestions control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl divQuestions;
+        
+        /// <summary>
         /// rptQuestions control.
         /// </summary>
         /// <remarks>
@@ -74,6 +83,78 @@ namespace LarpPortal {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Repeater rptQuestions;
+        
+        /// <summary>
+        /// pnlStaffComments control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel pnlStaffComments;
+        
+        /// <summary>
+        /// tbStaffComment control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tbStaffComment;
+        
+        /// <summary>
+        /// lblStaffComment control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblStaffComment;
+        
+        /// <summary>
+        /// mvCPAwarded control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.MultiView mvCPAwarded;
+        
+        /// <summary>
+        /// vwCPAwardedEntry control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.View vwCPAwardedEntry;
+        
+        /// <summary>
+        /// tbCPAwarded control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tbCPAwarded;
+        
+        /// <summary>
+        /// vwCPAwardedDisplay control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.View vwCPAwardedDisplay;
+        
+        /// <summary>
+        /// lblCPAwarded control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblCPAwarded;
         
         /// <summary>
         /// btnCancel control.
