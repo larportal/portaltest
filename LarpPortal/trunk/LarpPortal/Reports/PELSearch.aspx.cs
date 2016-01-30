67b64fe390b1f54163e536685ebad8d8fc9727fe
ryulin <ryulin@0f523e2a-70e0-4643-9f7c-8b595f397346>
2016-01-30 15:08:11 +0000

    -Report changes -Madrigal landing page

---
+++ LarpPortal/trunk/LarpPortal/Reports/PELSearch.aspx.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Data;
+using System.Data.SqlClient;
+using System.Reflection;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using LarpPortal.Classes;
+using System.Web.UI.HtmlControls;
+using System.IO;
+using System.Text;
+
+namespace LarpPortal.Reports
+{
+    public partial class PELSearch : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Page_PreRender(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                //FillGrid();
+            }
+        }
+
+        protected void gvPELSearch_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+
+        }
+
+        protected void FillGrid()
+        {
+            //int iTemp = 0;
+            int CampaignID = 0;
+            string Keyword1;
+             Keyword1 = "xxxxxx";           
+            if (txtKeyword.Text != null)
+                Keyword1 = txtKeyword.Text;
+            int UserID = 0;
+            string UserName;
+            string stStoredProc = "uspGetPELsWithKeywords";
+            string stCallingMethod = "PELSearch.FillGrid";
+            DateTime StartDate = DateTime.Today;
+            DateTime EndDate = DateTime.Today;
+            DataTable dtPELSearch = new DataTable();
+            SortedList sParams = new SortedList();
+            if (Session["UserID"].ToString() != null)
+            {
+                int.TryParse(Session["UserID"].ToString(), out UserID);
+            }
+            if (Session["UserName"] != null)
+            {
+                UserName = Session["UserName"].ToString();
+            }
+            else
+            {
+                UserName = "Reporting";
+            }
+            int.TryParse(Session["CampaignID"].ToString(), out CampaignID);
+            sParams.Add("@CampaignID", CampaignID);
+            sParams.Add("@Keyword1", Keyword1);
+            //sParams.Add("@StartDate", );
+            //sParams.Add("@EndDate", );
+            //sParams.Add("@EventID", );
+            sParams.Add("@PadLength", 30);
+            dtPELSearch = Classes.cUtilities.LoadDataTable(stStoredProc, sParams, "LARPortal", UserName, stCallingMethod);
+            gvPELSearch.DataSource = dtPELSearch;
+            gvPELSearch.DataBind();
+        }
+
+        protected void btnRunReport_Click(object sender, EventArgs e)
+        {
+            //btnExportCSV.Visible = true;
+            btnExportExcel.Visible = true;
+            FillGrid();
+            pnlReportOutput.Visible = true;
+        }
+
+        protected void btnExportExcel_Click(object sender, EventArgs e)
+        {
+            HtmlForm form = new HtmlForm();
+            Response.Clear();
+            Response.Buffer = true;
+            Response.Charset = "";
+            Response.AddHeader("content-disposition", string.Format("attachment;filename={0}", "LARPCalendar.xls"));
+            Response.ContentType = "application/ms-excel";
+            StringWriter sw = new StringWriter();
+            HtmlTextWriter hw = new HtmlTextWriter(sw);
+            gvPELSearch.AllowPaging = false;
+            //BindGridDetails(gvPELSearch);
+            form.Attributes["runat"] = "server";
+            form.Controls.Add(gvPELSearch);
+            this.Controls.Add(form);
+            form.RenderControl(hw);
+            string style = @"<!--mce:2-->";
+            Response.Write(style);
+            Response.Output.Write(sw.ToString());
+            Response.Flush();
+            Response.End();
+        }
+
+        protected void btnExportCSV_Click(object sender, EventArgs e)
+        {
+            //BindGridDetails(gvPELSearch);
+            Response.Clear();
+            Response.Buffer = true;
+            Response.AddHeader("content-disposition", "attachment;filename=LARPCalendar.csv");
+            Response.Charset = "";
+            Response.ContentType = "application/text";
+            gvPELSearch.AllowPaging = false;
+            gvPELSearch.DataBind();
+            StringBuilder sb = new StringBuilder();
+            for (int k = 0; k < gvPELSearch.Columns.Count; k++)
+            {
+                //add separator 
+                sb.Append(gvPELSearch.Columns[k].HeaderText + ',');
+            }
+            //append new line 
+            sb.Append("\r\n");
+            for (int i = 0; i < gvPELSearch.Rows.Count; i++)
+            {
+                for (int k = 0; k < gvPELSearch.Columns.Count; k++)
+                {
+                    //add separator 
+                    sb.Append(gvPELSearch.Rows[i].Cells[k].Text + ',');
+                }
+                //append new line 
+                sb.Append("\r\n");
+            }
+            Response.Output.Write(sb.ToString());
+            Response.Flush();
+            Response.End();
+        }
+    }
+}
