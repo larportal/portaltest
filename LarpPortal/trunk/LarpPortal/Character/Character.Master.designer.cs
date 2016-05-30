c933bcac29497912770ff4ad25918086bada6875
jbradshaw <jbradshaw@0f523e2a-70e0-4643-9f7c-8b595f397346>
2016-05-30 18:44:07 +0000

    Added Character Card Order.

--- LarpPortal/trunk/LarpPortal/Character/Character.Master.designer.cs
+++ LarpPortal/trunk/LarpPortal/Character/Character.Master.designer.cs
@@ -139,6 +139,15 @@ namespace LarpPortal.Character {
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl liCardCust;
         
         /// <summary>
+        /// liCharCharOrder control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl liCharCharOrder;
+        
+        /// <summary>
         /// liCharacterCard control.
         /// </summary>
         /// <remarks>
